0511ea9a5ee2f764e563d0f03db1458bc108396e
Mau <mauvepinktree@gmail.com>
2025-04-06 20:50:08 -0300

    add ball logic

--- Assets/Scripts/BallLogic.cs
+++ Assets/Scripts/BallLogic.cs
@@ -2,15 +2,28 @@ using UnityEngine;
 
 public class BallLogic : MonoBehaviour
 {
-
-    private void Awake()
-    {
-
-    }
+    [SerializeField]
+    private Vector3 velocity = new Vector3(3, 2, 0);
 
     private void Update()
     {
+        this.transform.position += velocity * Time.deltaTime;
+    }
 
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Overlapped with " + other.name + "!!");
+        Vector3 contactPoint = other.ClosestPoint(transform.position);
+        Vector3 direction = contactPoint - transform.position;
+
+        if (Mathf.Abs(direction.x) > Mathf.Abs(direction.y) && Mathf.Abs(direction.x) > Mathf.Abs(direction.z))
+        {
+            velocity.x = -velocity.x;
+        }
+        else
+        {
+            velocity.y = -velocity.y;
+        }
     }
 
 }
