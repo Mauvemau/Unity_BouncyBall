94355056e962a670090e3c2c0ba8fc42416be8c8
Mau <mauvepinktree@gmail.com>
2025-04-06 18:36:19 -0300

    create project

---
+++ Assets/Scripts/BallLogic.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class BallLogic : MonoBehaviour
+{
+
+    private void Awake()
+    {
+
+    }
+
+    private void Update()
+    {
+
+    }
+
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+A simple practice project
